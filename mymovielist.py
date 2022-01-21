91225bf0764239a010965e39b923a3cef3f1f76f
flash-ost <dotoner555@gmail.com>
2022-01-21 23:16:28 +0200

    Enable logging

--- mymovielist.py
+++ mymovielist.py
@@ -48,4 +48,21 @@ def create_app():
     api.add_resource(User, '/user')
     app.register_blueprint(api_bp)
 
+    # Enable logging in production mode
+    import logging
+    import os
+    from logging.handlers import RotatingFileHandler
+    if not app.debug:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/mymovielist.log', maxBytes=10240,
+                                        backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('MyMovieList')
+
     return app
